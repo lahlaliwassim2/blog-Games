a74884d94dd15914f48953311f5a74f4d3972dd9
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-05 15:19:31 +0100

    ajouter le composant app et liaison de projet avec bootstrap via les cdn

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <> 
+    <div className='h1'>Test Component de bas</div>
+    <h3>test component </h3>
+    </>
+  
+  )
+}
+
+export default App
