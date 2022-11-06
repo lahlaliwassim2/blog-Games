43e49d51f951c35b0d464394e9c7040308908e4d
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-06 14:09:55 +0100

    ak=jouter le composant qui contient la carde et ajouter aussi du style css

---
+++ src/components/Card/Card.js
@@ -0,0 +1,27 @@
+import './Card.css'
+import popular_01  from '../../assets/images/popular-01.jpg'
+
+
+const Card = () => {
+  return (
+    <>
+     <div className='most-pop-item'>
+                    <div className='card-wrapper'>
+                        <img className='most-pop-item-img' src={ popular_01 } />
+                        <div className='most-pop-item-content'>
+                            <h4 className='most-pop-item-titre'>
+                                7abahh <br /> 
+                                <span>Sandbox</span>
+                            </h4> 
+                            <ul>
+                                <li>4.8</li>
+                                <li>2.1</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+    </>
+  )
+}
+
+export default Card
