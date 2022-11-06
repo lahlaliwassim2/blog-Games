6e5f71acb810b16cb296549dfbdb72fc91ee6600
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-06 11:44:13 +0100

    ajouter des styles css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,12 @@ import NavItem from '../navItem/NavItem'
 
 const Header = () => {
   return (
-    <div className="navbar navbar-expand-md   bg-dark">
+    <div className="navbar was-navbar navbar-expand-md ">
     <div className="container">
       <a className="navbar-brand" href="#">
         <img src={logo} />
       </a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler color-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
