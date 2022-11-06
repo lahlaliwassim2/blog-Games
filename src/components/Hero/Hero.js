787a66b23ee083f89b864af32bba62b81d5265ae
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-06 01:46:40 +0100

    modification sur le style de comoposant Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <div className='hero-main'>
         <div className='hero-text'>
-            <h6 className='hero-subtitle'>welcom to cybrog</h6>
-            <h4 className='hero-title'><em>Browse</em>Vous trouver ous vos jux prefere là</h4>
+            <h6 className='hero-subtitle'>welcom to YouGame</h6>
+            <h4 className='hero-title'><em>Trouvez </em>vos jeux populaires ici</h4>
             <div className='main-button'>
                 <a href="#"> Browse Now</a>
             </div>
