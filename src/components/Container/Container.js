44558dff9d295b1583ccf049c99782dd6d8ebe6a
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-06 11:43:49 +0100

    ajouter des classes bootstrap pour redomenssionner le container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,7 +3,9 @@ import React from 'react'
 
 const Container = (props) => {
   return (
-    <div>{props.children}</div>
+    <div className='container main-container'>
+      {props.children}
+    </div>
   )
 }
 
