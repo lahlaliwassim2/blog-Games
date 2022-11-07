25120ec0d00fc160730ea616098306d3bec275b0
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-07 23:01:33 +0100

    ajouter les proprietées de la darde de manire dynamique

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,4 @@
 import './Card.css'
-import popular_01  from '../../assets/images/popular-01.jpg'
 
 
 const Card = (props) => {
@@ -7,15 +6,15 @@ const Card = (props) => {
     <>
      <div className='most-pop-item'>
                     <div className='card-wrapper'>
-                        <img className='most-pop-item-img' src={ popular_01 } />
+                        <img className='most-pop-item-img' src={props.image} />
                         <div className='most-pop-item-content'>
                             <h4 className='most-pop-item-titre'>
                                 {props.titre} <br /> 
-                                <span>Sandbox</span>
+                                <span>{props.category}</span>
                             </h4> 
                             <ul>
-                                <li>4.8</li>
-                                <li>2.1</li>
+                                <li>{props.rate}</li>
+                                <li>{props.download}</li>
                             </ul>
                         </div>
                     </div>
--- src/sections/MostPopulaire/MostPopulaire.js
+++ src/sections/MostPopulaire/MostPopulaire.js
@@ -1,4 +1,8 @@
 import './MostPopulaire.css'
+import popular_01  from '../../assets/images/popular-01.jpg'
+import popular_02  from '../../assets/images/popular-02.jpg'
+import popular_03  from '../../assets/images/popular-03.jpg'
+import popular_04  from '../../assets/images/popular-04.jpg'
 
 import { Card } from '../../components'
 const MostPopulaire = () => {
@@ -9,11 +13,10 @@ const MostPopulaire = () => {
                 <h4>Most Populaire</h4>
             </div>
             <div className='most-pop-items'>
-                <Card />
-                <Card />
-                 <Card />
-                 
-                <Card />
+                <Card image={popular_01}   titre="pubg"         category="action"   rate="3.5"   download="2.5M" />
+                <Card image={popular_02}   titre="fifa"         category="sport"    rate="1.4"   download="1.8M"/>
+                 <Card image={popular_03}  titre="valorant"     category="action"   rate="6.5"   download="4.2M" />
+                <Card image={popular_04}   titre="lol"          category="action"   rate="2.5"   download="7.1M" />
                  
             </div>
         </div>
